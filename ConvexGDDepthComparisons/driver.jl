9bfed9ed79984affdfdefe304cfd29ca35517fb5
Brian Timar <timarbrian@gmail.com>
2019-09-12 16:01:43 -0400

    settings

--- ConvexGDDepthComparisons/driver.jl
+++ ConvexGDDepthComparisons/driver.jl
@@ -2,4 +2,4 @@ using Distributed
 @everywhere include("settings.jl")
 #collect all training settings
 indices = [(di, ii, s) for di in 1:ndepth for ii in 1:numinit for s in 1:numseed]
-pmap(savetrace, indices[1:2])
+pmap(savetrace, indices)
