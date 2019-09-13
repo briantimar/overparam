1cf1f97767bc39a972650942081057edf9630f63
Brian Timar <timarbrian@gmail.com>
2019-09-12 20:00:31 -0400

    added lr sweep

--- ConvexGDDepthComparisons/driver.jl
+++ ConvexGDDepthComparisons/driver.jl
@@ -1,5 +1,5 @@
 using Distributed
 @everywhere include("settings.jl")
 #collect all training settings
-indices = [(di, ii, s) for di in 1:ndepth for ii in 1:numinit for s in 1:numseed]
+indices = [(di, ii, il, s) for di in 1:numdepth for ii in 1:numinit for il in 1:numlr for s in 1:numseed]
 pmap(savetrace, indices)
--- ConvexGDDepthComparisons/settings.jl
+++ ConvexGDDepthComparisons/settings.jl
@@ -7,35 +7,37 @@ Random.seed!(42)
 target = [.5 .1]
 
 # initialization values to try
-numinit = 5
+numinit = 20
 initializations = randn(numinit, 2)
 #number of seeds per model and initialization
-numseed = 50
+numseed = 100
 
 #depths to try (number of matrix multiplies)
-depths = collect(1:50)
-ndepth = length(depths)
+depths = collect(1:25)
+numdepth = length(depths)
 #width of intermediate layers
 width = 10
 
 #training settings
-lr = 1e-2
+lrvals = 10 .^ [-5, -4, -3, -2, -1]
+numlr = length(lrvals)
 tol = 1e-4
-numstep=50000
+numstep=Int(1e9)
 
-function filename(depthIndex::Int, initIndex::Int, seed::Int)
-    "data/trajectory_depthindex_$(depthIndex)_initindex_$(initIndex)_seed_$(seed).txt"
+function filename(depthIndex::Int, initIndex::Int, lrindex::Int, seed::Int)
+    "data/trajectory_depthindex_$(depthIndex)_initindex_$(initIndex)_lrindex_$(lrindex)_seed_$(seed).txt"
 end
 
-function savetrace(depthIndex::Int, initIndex::Int, seed::Int)
+function savetrace(depthIndex::Int, initIndex::Int, lrindex::Int, seed::Int)
     #sizes of the LNN layers
     depth = depths[depthIndex]
     sizes = vcat([2], repeat([width], depth-1), [1])
-    fname = filename(depthIndex, initIndex, seed)
+    fname = filename(depthIndex, initIndex, lrindex, seed)
+    lr = lrvals[lrindex]
     init = reshape(initializations[initIndex, :], (1, 2))
     saveconvexGDtrace(sizes, target, init, fname, 
                         lr=lr, tol=tol, numstep=numstep)
 end
 
-savetrace(indices::Tuple{Int, Int, Int}) = savetrace(indices...)
+savetrace(indices::Tuple{Int, Int, Int, Int}) = savetrace(indices...)
 
