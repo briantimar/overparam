b90641da02253363638d778ecd321609401b8ae5
Brian Timar <timarbrian@gmail.com>
2019-09-12 21:05:28 -0400

    same

--- ConvexGDDepthComparisons/compileData.jl
+++ ConvexGDDepthComparisons/compileData.jl
@@ -8,15 +8,16 @@ for ilr in 1:numlr
     @info "Collecting lr index $ilr..."
     for ii in 1:numinit
         for id in 1:numdepth
-
             traces = Vector{Union{Array{Float64, 2}, Nothing}}()
             for s in 1:numseed
                 try
                     t = readdlm(filename(id, ii, ilr, s))
+                    push!(traces, t)
                 catch
                     t = nothing
+                    push!(traces, t)
                 end
-                push!(traces, t)
+                
             end
             alldata["traces_depthindex_$(id)_initindex_$(ii)_lrindex_$(ilr)"] = traces
         end
