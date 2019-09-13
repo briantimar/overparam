182557787df1504aeaf0f64d0c22350341dad9a8
Brian Timar <timarbrian@gmail.com>
2019-09-12 23:04:30 -0400

    update compile

--- ConvexGDDepthComparisons/compileData.jl
+++ ConvexGDDepthComparisons/compileData.jl
@@ -3,29 +3,36 @@ include("settings.jl")
 using DelimitedFiles
 using JLD
 
-alldata = Dict{String, Any}()
+#to save space only pack a few traces per setting
+numtrace=1
+
 for ilr in 1:numlr
     @info "Collecting lr index $ilr..."
+    alldata = Dict{String, Any}()
     for ii in 1:numinit
         for id in 1:numdepth
-            traces = Vector{Union{Array{Float64, 2}, Nothing}}()
+            traces = Vector{Array{Float64, 2}}()
+            lengths = Vector{Int}()           
             for s in 1:numseed
                 try
                     t = readdlm(filename(id, ii, ilr, s))
-                    push!(traces, t)
+                    push!(lengths, length(t))
+                    length(traces) < numtrace && push!(traces, t)
                 catch
                     t = nothing
-                    push!(traces, t)
+                    length(traces) < numtrace && push!(traces, t)
                 end
                 
             end
-            alldata["traces_depthindex_$(id)_initindex_$(ii)_lrindex_$(ilr)"] = traces
+            alldata["traces_depthindex_$(id)_initindex_$(ii)"] = traces
+            alldata["lengths_depthindex_$(id)_initindex_$(ii)"] = lengths
         end
     end
+    for var in [:numseed, :numinit, :numdepth, :numlr, :initializations, :depths, :tol, :width, :numstep, :target]
+        alldata["$var"] = eval(var)
+    end
+    alldata["lr"] = lrvals[ilr]
+    save("data/alldata_lrindex_$(ilr).jld", alldata)
 end
 
-for var in [:numseed, :numinit, :numdepth, :numlr, :initializations, :depths, :lrvals, :tol, :width, :numstep, :target]
-    alldata["$var"] = eval(var)
-end
-
-save("data/alldata.jld", alldata)
+@info "Done"
