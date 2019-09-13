9572fe8286413c07530d8bf7a86f53169d90539b
Brian Timar <timarbrian@gmail.com>
2019-09-12 21:02:27 -0400

    update compiledata

--- ConvexGDDepthComparisons/compileData.jl
+++ ConvexGDDepthComparisons/compileData.jl
@@ -7,16 +7,22 @@ alldata = Dict{String, Any}()
 
 for ii in 1:numinit
     for id in 1:ndepth
-        traces = Vector{Array{Float64, 2}}()
-        for s in 1:numseed
-            t = readdlm(filename(id, ii, s))
-            push!(traces, t)
+        for ilr in 1:numlr
+            traces = Vector{Union{Array{Float64, 2}, Nothing}}()
+            for s in 1:numseed
+                try
+                    t = readdlm(filename(id, ii, ilr, s))
+                catch
+                    t = nothing
+                end
+                push!(traces, t)
+            end
+            alldata["traces_depthindex_$(id)_initindex_$(ii)_lrindex_$(ilr)"] = traces
         end
-        alldata["traces_depthindex_$(id)_initindex_$(ii)"] = traces
     end
 end
 
-for var in [:numseed, :numinit, :ndepth, :initializations, :depths, :lr, :tol, :width, :numstep, :target]
+for var in [:numseed, :numinit, :numdepth, :numlr :initializations, :depths, :lrvals, :tol, :width, :numstep, :target]
     alldata["$var"] = eval(var)
 end
 
